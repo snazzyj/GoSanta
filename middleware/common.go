0e3f44a1e48354e85f750b4b537bf527bda69432
Alex Olson <silentx.alex@gmail.com>
2024-11-04 13:10:14 -0500

    removed log

--- middleware/common.go
+++ middleware/common.go
@@ -1,14 +1,12 @@
 package middleware
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func AddCommonFieldsToRequest(g *gin.Context) {
-	fmt.Println("time.Now()", time.Now())
 	g.Set("createdAt", time.Now())
 	g.Set("createdBy", "API")
 	g.Next()
